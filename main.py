7b247287e50f2a03e88b3e599dc210925f146b5a
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-28 20:01:48 -0700

    first

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
     number = int(input('Enter your input: '))
     result = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
+    while number >= 2:
+        remainder = number % 2
+        result.append(remainder)
+        number = number // 2
+    result.append(number)
+    
 
     print(*result)
     ########################################
